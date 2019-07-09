13d2becc943982a3febf0aefaf3cf46fe34b2bfe
Silent <zdanio95@gmail.com>
2019-07-09 19:32:44 +0200

    Move to /MT and disable debug documents path for shipping

--- SilentPatchMGR/SilentPatchMGR.cpp
+++ SilentPatchMGR/SilentPatchMGR.cpp
@@ -19,7 +19,7 @@
 #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #pragma comment(lib, "Comctl32.lib")
 
-#define DEBUG_DOCUMENTS_PATH	1
+#define DEBUG_DOCUMENTS_PATH	0
 
 HMODULE hDLLModule;
 
